547a2465ea8e87b821788964b99d1498826c20d5
Jabobe17 <jakob.seeberger@student.htl-rankweil.at>
2026-06-16 20:00:42 +0200

    Durch ein Merge conflict hat es uns das Programm irgendwie zerschossen habe jetzt alles wieder gerichtet

--- auth.py
+++ auth.py
@@ -1,10 +1,9 @@
 from fastapi import Header, HTTPException, status
 
 API_KEYS = {
-    "admin-key": "admin",
-    "user-key": "user"
+    "admin": "admin",
+    "user": "user"
 }
-
 
 
 def get_current_role(x_api_key: str = Header(None)):
@@ -25,12 +24,9 @@ def get_current_role(x_api_key: str = Header(None)):
     return role
 
 
-
-
 def require_admin(role: str):
     if role != "admin":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Keine Berechtigung. Nur Admins dürfen diese Operation ausführen."
         )
-
